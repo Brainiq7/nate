9f09e859073d8a5af31c8ed4b406d8c0ca18ba72
Precious Jahlom Agboado <jahlomp@gmail.com>
2015-05-15 23:42:06 +0000

    adding some build options

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,27 +14,79 @@ module.exports = function(grunt) {
 	        platforms: ['win', 'linux', 'osx'],
 	        buildDir: '<%= config.webkitbuilds %>', // Where the build version of my node-webkit app is saved
 	    },
-	    src: ['<%= config.src %>/**/*'] // Your node-webkit app
+	    src: ['<%= config.build %>/**/*'] // Your node-webkit app
 	  },
     copy: {
-      data: {
+      main: {
         expand: true,
         cwd: '<%= config.src %>/',
-        src: ['package.json'],
+        src: ['package.json', 'main.html'],
         dest: '<%= config.dist %>/'
+      },
+      style: {
+        expand: true,
+        cwd: '<%= config.src %>/less/',
+        src: ['*.less'],
+        dest: '<%= config.dist %>/less/'
+      },
+      less: {
+        expand: true,
+        cwd: '<%= config.src %>/bower_components/less/dist/',
+        src: ['less.min.js'],
+        dest: '<%= config.dist %>/bower_components/less/dist/'
+      },
+      zepto: {
+        expand: true,
+        cwd: '<%= config.src %>/bower_components/zepto/',
+        src: ['zepto.min.js'],
+        dest: '<%= config.dist %>/bower_components/zepto/'
+      },
+      ace: {
+        expand: true,
+        cwd: '<%= config.src %>/js/ace-builds/src-min-noconflict/',
+        src: ['**/*.js'],
+        dest: '<%= config.dist %>/js/ace-builds/src-min-noconflict/'
+      },
+      js: {
+        expand: true,
+        cwd: '<%= config.src %>/js/',
+        src: ['*.js'],
+        dest: '<%= config.dist %>/js/'
+      },
+      node_modules: {
+        expand: true,
+        cwd: 'node_modules/',
+        src: ['node-watch/**/*'],
+        dest: '<%= config.dist %>/node_modules/'
+      }
+    },
+    clean: {
+      build: {
+        files: [
+          {
+            expand: true,
+            cwd: 'build/',
+            src: '**/*',
+          },
+        ],
       }
     },
     watch: {
       data: {
-        files: ['<%= config.src %>*.json'],
+        files: [
+          'node_modules/',
+          '<%= config.src %>/**/*'
+            
+        ],
         tasks: ['copy']
       }
     }
   });
 
   require('load-grunt-tasks')(grunt);
-
-  grunt.registerTask('build', ['nodewebkit']);
-  grunt.registerTask('default', ['build', 'watch']);
+  
+  grunt.registerTask('build', ['clean', 'copy']);
+  grunt.registerTask('release', ['build', 'nodewebkit']);
+  grunt.registerTask('default', ['release', 'watch']);
 
 };

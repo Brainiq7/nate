e5f10325d4971669e01147d49b30ffcdc6384d0a
Precious Jahlom Agboado <jahlomp@gmail.com>
2015-05-22 20:23:25 +0000

    added code mirror editor

--- src/js/Nate.js
+++ src/js/Nate.js
@@ -1,7 +1,154 @@
-var gui = require('nw.gui');
-var win = gui.Window.get();
+var newButton, openButton, saveButton;
+var editor;
+var menu;
+var fileEntry;
+var hasWriteAccess;
 
-initMenus = function() {
+var gui = require("nw.gui");
+var fs = require("fs");
+var win = gui.Window.get();
+var clipboard = gui.Clipboard.get();
+
+function handleDocumentChange(title) {
+  var mode = "javascript";
+  var modeName = "JavaScript";
+  if (title) {
+    title = title.match(/[^/]+$/)[0];
+    document.getElementById("title").innerHTML = title;
+    document.title = title;
+    if (title.match(/.json$/)) {
+      mode = {name: "javascript", json: true};
+      modeName = "JavaScript (JSON)";
+    } else if (title.match(/.html$/)) {
+      mode = "htmlmixed";
+      modeName = "HTML";
+    } else if (title.match(/.css$/)) {
+      mode = "css";
+      modeName = "CSS";
+    }
+  } else {
+    document.getElementById("title").innerHTML = "[no document loaded]";
+  }
+  editor.setOption("mode", mode);
+  document.getElementById("mode").innerHTML = modeName;
+}
+
+function newFile() {
+  fileEntry = null;
+  hasWriteAccess = false;
+  handleDocumentChange(null);
+}
+
+function setFile(theFileEntry, isWritable) {
+  fileEntry = theFileEntry;
+  hasWriteAccess = isWritable;
+}
+
+function readFileIntoEditor(theFileEntry) {
+  fs.readFile(theFileEntry, function (err, data) {
+    if (err) {
+      console.log("Read failed: " + err);
+    }
+
+    handleDocumentChange(theFileEntry);
+    editor.setValue(String(data));
+  });
+}
+
+function writeEditorToFile(theFileEntry) {
+  var str = editor.getValue();
+  fs.writeFile(theFileEntry, editor.getValue(), function (err) {
+    if (err) {
+      console.log("Write failed: " + err);
+      return;
+    }
+
+    handleDocumentChange(theFileEntry);
+    console.log("Write completed.");
+  });
+}
+
+var onChosenFileToOpen = function(theFileEntry) {
+  setFile(theFileEntry, false);
+  readFileIntoEditor(theFileEntry);
+};
+
+var onChosenFileToSave = function(theFileEntry) {
+  setFile(theFileEntry, true);
+  writeEditorToFile(theFileEntry);
+};
+
+function handleNewButton() {
+  if (false) {
+    newFile();
+    editor.setValue("");
+  } else {
+    var x = window.screenX + 10;
+    var y = window.screenY + 10;
+    window.open('main.html', '_blank', 'screenX=' + x + ',screenY=' + y);
+  }
+}
+
+function handleOpenButton() {
+  $("#openFile").trigger("click");
+}
+
+function handleSaveButton() {
+  if (fileEntry && hasWriteAccess) {
+    writeEditorToFile(fileEntry);
+  } else {
+    $("#saveFile").trigger("click");
+  }
+}
+
+function initContextMenu() {
+  menu = new gui.Menu();
+  menu.append(new gui.MenuItem({
+    label: 'Copy',
+    click: function() {
+      clipboard.set(editor.getSelection());
+    }
+  }));
+  menu.append(new gui.MenuItem({
+    label: 'Cut',
+    click: function() {
+      clipboard.set(editor.getSelection());
+      editor.replaceSelection('');
+    }
+  }));
+  menu.append(new gui.MenuItem({
+    label: 'Paste',
+    click: function() {
+      editor.replaceSelection(clipboard.get());
+    }
+  }));
+
+  document.getElementById("editor").addEventListener('contextmenu',
+    function(ev) { 
+    ev.preventDefault();
+    menu.popup(ev.x, ev.y);
+    return false;
+  });
+}
+function openFile() {
+    $("#openFile").trigger("click");
+};
+
+function newWindow() {
+    var x = window.screenX + 10;
+    var y = window.screenY + 10;
+    window.open('main.html', '_blank', 'screenX=' + x + ',screenY=' + y);
+};
+
+function closeAllWindows() {
+    gui.App.closeAllWindows();
+};
+
+function closeWindow() {
+    win.close();
+};
+
+function initMenubar() {
 
     var nativeMenuBar = new gui.Menu({
         type: "menubar"
@@ -16,19 +163,11 @@ initMenus = function() {
 
     //  Submenus for File Menu
     menuFileMain.append(new gui.MenuItem({
-        label: "New File",
-        key: "n",
-        modifiers: "ctrl",
-        click: function() {
-            
-        }
-    }));
-    menuFileMain.append(new gui.MenuItem({
         label: "Open",
         key: "o",
         modifiers: "ctrl",
         click: function() {
-            
+            openFile()
         }
     }));
     menuFileMain.append(new gui.MenuItem({ 
@@ -58,7 +197,7 @@ initMenus = function() {
         key: "n",
         modifiers: "ctrl-shift",
         click: function() {
-            
+            newWindow()
         }
     }));
     menuFileMain.append(new gui.MenuItem({
@@ -66,7 +205,7 @@ initMenus = function() {
         key: "w",
         modifiers: "ctrl-shift",
         click: function() {
-            
+            closeWindow()
         }
     }));
     menuFileMain.append(new gui.MenuItem({ 
@@ -75,7 +214,7 @@ initMenus = function() {
     menuFileMain.append(new gui.MenuItem({
         label: "Exit",
         click: function() {
-            
+            closeAllWindows()
         }
     }));
 
@@ -258,7 +397,46 @@ initMenus = function() {
 };
 
 onload = function() {
-	initMenus()
-	win.maximize();
-  	win.show();
+    initContextMenu();
+    initMenubar()
+
+    $("#saveFile").change(function(evt) {
+        onChosenFileToSave($(this).val());
+    });
+    $("#openFile").change(function(evt) {
+        onChosenFileToOpen($(this).val());
+    });
+
+    editor = CodeMirror(
+        document.getElementById("editor"),{
+            mode: {name: "javascript", json: true },
+            lineNumbers: true,
+            lineWrapping: true,
+            indentUnit: 4,
+            tabSize: 4,
+            autofocus: true,
+            theme: "ambiance",
+            extraKeys: {
+                "Cmd-S": function(instance) { handleSaveButton() },
+                "Ctrl-S": function(instance) { handleSaveButton() },
+            }
+        }
+    );
+
+  newFile();
+  onresize();
+
+  win.show();
 };
+
+onresize = function() {
+  var container = document.getElementById('editor');
+  var containerWidth = container.offsetWidth;
+  var containerHeight = container.offsetHeight;
+
+  var scrollerElement = editor.getScrollerElement();
+  scrollerElement.style.width = containerWidth + 'px';
+  scrollerElement.style.height = containerHeight + 'px';
+
+  editor.refresh();
+}
